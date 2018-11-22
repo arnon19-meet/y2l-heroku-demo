723c61ca22aefac3ed38ee319cb880c8b03fbb10
aboud20-meet <aboud20@meet.mit.edu>
2018-11-22 18:06:10 +0200

    F

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
 from flask import Flask
 from flask import render_template, request
-from database import *
+from database import * 
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'YOUR-VERY-SECRET-SHHH'
 
 @app.route('/')
-def hello_world():
+def catbook_home():
     cats = get_all_cats()
     return render_template("home.html", cats=cats)
-if __name__ == '__main__':
-    app.run(debug=True)
+    
+@app.route('/cats/<int:id>')
+def name_route(id):
+    cat=get_cat(id)
+    print("---")
+    print(cat)
+    return render_template("cat.html", cat=cat)
 
+
+@app.route("/response", methods=['GET','POST'])
+def addcat():
+    if request.method == 'GET' :
+        return render_template("response.html")
+    else:
+        name=request.form['firstname']
+        create_cat(name,0)
+        cats = get_all_cats()
+        return render_template('home.html',
+        cats = cats)
+
+@app.route("/vote", methods=['GET','POST'])
+def voteforcat():
+    if request.method == 'GET' :
+        cats = get_all_cats()
+        return render_template("vote.html",
+        cats = cats)
+    
+    else:
+        vote += 1
+        cats = get_all_cats()
+        return render_template('home.html',
+        cats = cats)
+
+if __name__ == '__main__':
+   app.run(debug = True)
---
+++ database.py
@@ -0,0 +1,23 @@
+from models import *
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///cats.db', connect_args={'check_same_thread': False})
+Base.metadata.create_all(engine)
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+def create_cat(name, vote):
+    cat_object = Cat(name=name,
+    vote = vote)
+    session.add(cat_object)
+    session.commit()
+
+def get_all_cats():
+    cats = session.query(Cat).all()
+    return cats
+
+def get_cat(id):
+    cat_object = session.query(Cat).filter_by(id=id).first()
+    return cat_object    
